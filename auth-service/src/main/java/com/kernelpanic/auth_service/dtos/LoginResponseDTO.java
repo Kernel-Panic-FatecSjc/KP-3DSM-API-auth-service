4c374efb73f1f5425ecb21af025ded02ad2a4acb
Vinícius Leite <163305926+vinislvleite@users.noreply.github.com>
2026-04-30 17:26:17 -0300

    chore(not-us): Adicionando o campo de nome no DTO de loginresponse

--- auth-service/src/main/java/com/kernelpanic/auth_service/dtos/LoginResponseDTO.java
+++ auth-service/src/main/java/com/kernelpanic/auth_service/dtos/LoginResponseDTO.java
@@ -6,6 +6,7 @@ import lombok.Data;
 public class LoginResponseDTO {
     private String id;
     private String token;
+    private String nome;
     private String email;
     private String cargo;
 }
