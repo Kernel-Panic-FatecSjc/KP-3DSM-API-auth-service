b0db4f35fd0ccb39da65badbce3d7f141a2f577d
Vinícius Leite <contato.vinips25@outlook.com>
2026-05-12 18:55:17 -0300

    feat: Arrumando mais 1 vez o login

--- auth-service/src/main/resources/data.sql
+++ auth-service/src/main/resources/data.sql
@@ -1,5 +1,45 @@
-INSERT INTO usuario (id, nome, email, senha, cargo, ativo, salario, gerente_id, data_criacao, data_atualizacao)
-VALUES 
-(1, 'Gestor', 'gestor@kernelpanic.com', '$2a$10$16e598HFycun0McHP9SRteQgnvbqhzZNlFkhEoIcmjXnrRRVW5p5q', 'ROLE_GESTOR', true, '100', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(2, 'Financeiro', 'financeiro@kernelpanic.com', '$2a$10$16e598HFycun0McHP9SRteQgnvbqhzZNlFkhEoIcmjXnrRRVW5p5q', 'ROLE_FINANCEIRO', true, '100', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP),
-(3, 'Profissional', 'profissional@kernelpanic.com', '$2a$10$16e598HFycun0McHP9SRteQgnvbqhzZNlFkhEoIcmjXnrRRVW5p5q', 'ROLE_PROFISSIONAL', true, '100', NULL, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT IGNORE INTO usuario (
+    nome,
+    email,
+    senha,
+    cargo,
+    ativo,
+    salario,
+    gerente_id,
+    data_criacao,
+    data_atualizacao
+)
+VALUES
+(
+    'Gestor',
+    'gestor@kernelpanic.com',
+    '$2a$10$16e598HFycun0McHP9SRteQgnvbqhzZNlFkhEoIcmjXnrRRVW5p5q',
+    'ROLE_GESTOR',
+    true,
+    '100',
+    NULL,
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+),
+(
+    'Financeiro',
+    'financeiro@kernelpanic.com',
+    '$2a$10$16e598HFycun0McHP9SRteQgnvbqhzZNlFkhEoIcmjXnrRRVW5p5q',
+    'ROLE_FINANCEIRO',
+    true,
+    '100',
+    NULL,
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+),
+(
+    'Profissional',
+    'profissional@kernelpanic.com',
+    '$2a$10$16e598HFycun0McHP9SRteQgnvbqhzZNlFkhEoIcmjXnrRRVW5p5q',
+    'ROLE_PROFISSIONAL',
+    true,
+    '100',
+    NULL,
+    CURRENT_TIMESTAMP,
+    CURRENT_TIMESTAMP
+);
